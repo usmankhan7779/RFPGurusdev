6b666f8cf772f9ee3d12f31fdcda35ee92960ca6
usmankhanbrainplow <35483640+usmankhanbrainplow@users.noreply.github.com>
2019-06-22 13:27:07 +0500

    hghjghj

--- src/app/components/other/pdf-viewer/pdf-viewer.component.ts
+++ src/app/components/other/pdf-viewer/pdf-viewer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-pdf-viewer',
@@ -10,17 +11,20 @@ export class PdfViewerComponent implements OnInit {
   a;
   b;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
   // https://storage.rfpgurus.com/bplrfpgurus/794940-Bid-Ad-Lake-Elmo-Regional-Park-Reserve-Improvements
   ngOnInit() {
     // alert(localStorage.getItem('web_info'))
-    // this.pdfSrc = localStorage.getItem('web_info')
-this.a = 'https://storage.rfpgurus.com/bplrfpgurus/'
-this.b='794940-Bid-Ad-Lake-Elmo-Regional-Park-Reserve-Improvements'
+    this.pdfSrc = localStorage.getItem('web_info')
+// this.a = 'https://storage.rfpgurus.com/bplrfpgurus/'
+// this.b='794940-Bid-Ad-Lake-Elmo-Regional-Park-Reserve-Improvements'
     // https://storage.rfpgurus.com/bplrfpgurus/794917-Water-10794916.zip
-    
-    this.pdfSrc = this.a +this.b
-    alert(this.pdfSrc)
+    // this.route.queryParams
+    // .subscribe(params => {
+    //   this.pdfSrc = params['query']
+    // })
+    // this.pdfSrc = this.a +this.b
+    // alert(this.pdfSrc)
     console.log(this.pdfSrc)
   }
 
