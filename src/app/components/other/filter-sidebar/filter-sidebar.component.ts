c87a4eabf41a2a074eb5347ca845d0db8c157134
Arhumbutt <46703032+Arhumbutt@users.noreply.github.com>
2019-06-28 17:00:08 +0500

    wfswff
    fsdf

--- src/app/components/other/filter-sidebar/filter-sidebar.component.ts
+++ src/app/components/other/filter-sidebar/filter-sidebar.component.ts
@@ -45,7 +45,6 @@ export class FilterSidebarComponent implements OnInit, OnDestroy {
   endRequest;
   constructor(private filterServ: FilterSidebarService, private homeServ: HomeService, private datePipe: DatePipe, public _shareData: SharedData, private _nav: Router, private advanceService: AdvanceService, private allCatService: AllCategoryService) {
     if (localStorage.getItem('status')) {
-      alert('status');
       this.status = localStorage.getItem('status');
     }
     if (localStorage.getItem('enterdate')) { this.enterdate = localStorage.getItem('enterdate') }
@@ -55,17 +54,8 @@ export class FilterSidebarComponent implements OnInit, OnDestroy {
     if (localStorage.getItem('states')) {
       this.states = localStorage.getItem('states');
     }
-    if (localStorage.getItem('agencies')) { 
-      alert('Agencies');
-            this.agencies = localStorage.getItem('agencies');
-            console.log(this.agencies)
-
-     }
-    if (localStorage.getItem('cates')) {
-       this.cates = localStorage.getItem('cates') ;
-       console.log(this.cates)
-
-      }
+    if (localStorage.getItem('agencies')) { this.agencies = localStorage.getItem('agencies') }
+    if (localStorage.getItem('cates')) { this.cates = localStorage.getItem('cates') }
     if (localStorage.getItem('subcat')) { this.subcates = localStorage.getItem('subcat') }
     this.allCatService.rfpcategory_subsat().subscribe(
       data => {
@@ -146,7 +136,7 @@ export class FilterSidebarComponent implements OnInit, OnDestroy {
             this.cat = data['Categories'];
           }
           if (data['Agencies']) {
-           
+            // this.agency = data['Agencies'];
             this.agency = data['Agencies'];
 
           }
@@ -177,7 +167,7 @@ export class FilterSidebarComponent implements OnInit, OnDestroy {
             this.cat = data['Categories'];
           }
           if (data['Agencies']) {
-           
+            // this.agency = data['Agencies'];
             this.agency = data['Agencies'];
   
           }
@@ -210,7 +200,7 @@ export class FilterSidebarComponent implements OnInit, OnDestroy {
     localStorage.removeItem('subcates');
     localStorage.removeItem('submission_from');
     localStorage.removeItem('submission_to');
-  
+    // console.log(this.status);
 f.resetForm();
   }
   
@@ -275,9 +265,7 @@ f;
   }
 
   ngOnInit() {
-  // this.f.resetForm();
     window.scroll(0, 0);
-    // this.formclear(this.f);
     if (localStorage.getItem('status')) { this.status = localStorage.getItem('status') }
     if (localStorage.getItem('enterdate')) { this.enterdate = localStorage.getItem('enterdate') }
     if (localStorage.getItem('duedate')) { this.duedate = localStorage.getItem('duedate') }
