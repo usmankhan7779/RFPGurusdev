75870b3cb2911d9bd24c9c99c4ffd7848ca00070
usmankhanbrainplow <35483640+usmankhanbrainplow@users.noreply.github.com>
2019-07-30 11:20:05 +0500

    aduasdada

---
+++ src/app/components/profile/addrfps/addrfps.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addrfps',
+  templateUrl: './addrfps.component.html',
+  styleUrls: ['./addrfps.component.css']
+})
+export class AddrfpsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
