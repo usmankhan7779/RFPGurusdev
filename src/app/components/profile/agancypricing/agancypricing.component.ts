86f030a9b1494d07f20f3bf80a9fdb6430d184c8
aqsach <35590350+aqsach@users.noreply.github.com>
2019-07-31 18:05:42 +0500

    pdf

--- src/app/components/profile/agancypricing/agancypricing.component.ts
+++ src/app/components/profile/agancypricing/agancypricing.component.ts
@@ -11,7 +11,8 @@ import { SignupService } from '../../Auth/signup/signup.service';
 import { HomeService } from '../../common/home/home.service';
 import { MainService } from 'src/app/services/main.service';
 declare var $: any;
-
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 @Component({
   selector: 'app-agancypricing',
   templateUrl: './agancypricing.component.html',
@@ -385,6 +386,24 @@ forms: FormGroup;
       event.preventDefault();
     }
   }
+  captureScreen() {
+    var data = document.getElementById('contentToConvert');
+    html2canvas(data).then(canvas => {
+        // Few necessary setting options  
+        var imgWidth = 208;
+        var pageHeight = 295;
+        // var imgHeight=200;
+        var imgHeight = canvas.height * imgWidth / canvas.width;
+        var heightLeft = imgHeight;
+
+        const contentDataURL = canvas.toDataURL('image/png')
+        let pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF  
+        var position = 0;
+        pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+        pdf.save('Invoice.pdf'); // Generated PDF   
+    });
+
+}
   cardtype
   ShowButton(var_type_atm) {
     this.cardtype = var_type_atm;
