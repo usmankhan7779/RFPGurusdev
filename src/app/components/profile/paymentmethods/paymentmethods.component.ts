97b4e906abc5f452d6f9046da3078f8a5a60d6c5
aqsach <aqsa.qayyum@brainplow.com>
2019-09-04 19:05:00 +0500

    uplaod payment method

--- src/app/components/profile/paymentmethods/paymentmethods.component.ts
+++ src/app/components/profile/paymentmethods/paymentmethods.component.ts
@@ -81,7 +81,7 @@ export class PaymentmethodsComponent implements OnInit, OnDestroy {
   ccv2;
   form: FormGroup;
   form2: FormGroup;
-
+  allcountry;
   cardnumber2;
   var_box_check: boolean = false;
   destroy_value;
@@ -96,6 +96,10 @@ export class PaymentmethodsComponent implements OnInit, OnDestroy {
     this.cardnumber2 = false;
     this.ccv = true;
     this.ccv2 = false;
+    this.signupServ.getcounty().subscribe( data =>{
+      this.allcountry = data['countries'];
+      console.log(this.allcountry);
+    })
   }
   ShowButton(var_type_atm, f: NgForm) {
     this.cardtype = var_type_atm;
